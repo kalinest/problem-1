c304680bf8fba76947a1ec1c3eb6a1e3a1501ed0
Alexandre Rademaker <arademaker@gmail.com>
2016-03-10 09:13:49 -0300

    code examples reading data

--- startkit.lisp
+++ startkit.lisp
@@ -1,8 +1,22 @@
 
 
-(defun count-inversions (filename)
+(defun read-data (filename)
+  (with-open-file (in filename)
+    (let ((res))
+      (do ((line (read-line in nil nil)
+		 (read-line in nil nil)))
+	  ((null line)
+	   (reverse res))
+	(push (parse-integer line) res)))))
+
+(defun count-inversions (alist)
   ...)
 
 
-; (count-inversions "IntegerArray.txt")
+(count-inversions (read-data "IntegerArray.txt"))
 
+(let ((lista (read-data "IntegerArray.txt")))
+  (count-inversions lista))
+
+(defvar lista (read-data "IntegerArray.txt"))
+(count-inversions lista)
