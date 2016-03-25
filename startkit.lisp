34e263977fa75e73cda8c2b3990053b91a5b6d04
Alexandre Rademaker <arademaker@gmail.com>
2016-03-24 22:17:09 -0300

    solucao problema 1

--- startkit.lisp
+++ startkit.lisp
@@ -1,5 +1,4 @@
 
-
 (defun read-data (filename)
   (with-open-file (in filename)
     (let ((res))
@@ -9,14 +8,30 @@
 	   (reverse res))
 	(push (parse-integer line) res)))))
 
-(defun count-inversions (alist)
-  ...)
+
+(defun merge-and-count (list1 list2 &optional (response nil) (counter 0))
+  (if (not (and list1 list2))
+      (values (append (reverse response) (or list1 list2)) counter)
+      (if (<= (car list1) (car list2))
+	  (merge-and-count (cdr list1) list2
+			   (cons (car list1) response)
+			   counter)
+	  (merge-and-count list1 (cdr list2)
+			   (cons (car list2) response)
+			   (+ counter (length list1))))))
 
 
-(count-inversions (read-data "IntegerArray.txt"))
+(defun sort-and-count (alist)
+  (let ((length (length alist)))
+    (if (equal length 1)
+	(values alist 0)
+	(multiple-value-bind (x b)
+	    (sort-and-count (subseq alist 0 (round length 2)))
+	  (multiple-value-bind (y c)
+	      (sort-and-count (subseq alist (round length 2)))
+	    (multiple-value-bind (z d)
+		(merge-and-count x y)
+	      (values z (+ b c d))))))))
 
-(let ((lista (read-data "IntegerArray.txt")))
-  (count-inversions lista))
 
-(defvar lista (read-data "IntegerArray.txt"))
-(count-inversions lista)
+
